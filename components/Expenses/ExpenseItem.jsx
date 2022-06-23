f76f2879c23c5675c9e82d99d3431a7510d2dee3
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-06-23 13:19:33 -0400

    initial

---
+++ components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,15 @@
+import ExpenseDate from './ExpenseDate';
+
+const ExpenseItem = (props) => {
+  return (
+      <div className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+        </div>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+  );
+}
+
+export default ExpenseItem;
