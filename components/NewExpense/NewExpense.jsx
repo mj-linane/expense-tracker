14eff9b9e28aff823d519fba0c9e5de33a16d1c0
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-06-23 14:14:48 -0400

    🖥️wip: pass down event handler

--- components/NewExpense/NewExpense.jsx
+++ components/NewExpense/NewExpense.jsx
@@ -1,27 +1,33 @@
-import React from 'react';
-import ExpenseForm from './ExpenseForm';
+import React from 'react'
+import ExpenseForm from './ExpenseForm'
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    }
+    props.onAddExpense(expenseData)
+  }
+
   return (
-      <div className="new-expense">
-        <ExpenseForm/>
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
 
-        <style jsx>{`
-          .new-expense {
-            background-color: #a892ee;
-            padding: 1rem;
-            margin: 2rem auto;
-            width: 50rem;
-            max-width: 95%;
-            border-radius: 12px;
-            text-align: center;
-            box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
-          }
+      <style jsx>{`
+        .new-expense {
+          background-color: #a892ee;
+          padding: 1rem;
+          margin: 2rem auto;
+          width: 50rem;
+          max-width: 95%;
+          border-radius: 12px;
+          text-align: center;
+          box-shadow: 0 1px 8px rgba(0, 0, 0, 0.25);
+        }
+      `}</style>
+    </div>
+  )
+}
 
-
-        `}</style>
-      </div>
-  );
-};
-
-export default NewExpense;
+export default NewExpense
--- pages/index.js
+++ pages/index.js
@@ -3,48 +3,60 @@ import styles from '../styles/Home.module.css'
 import Expenses from '../components/Expenses/Expenses';
 import NewExpense from '../components/NewExpense/NewExpense';
 
-const expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+export default function Home ()
+{
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
 
-export default function Home() {
+  const addExpenseHandler = expense =>
+  {
+    console.log("On home page")
+    console.log(expense)
+  }
+
   return (
-      <div className={styles.container}>
-        <Head>
-          <title>Expense App</title>
-          <meta name="description"
-                content="A simple expense tracker"/>
-        </Head>
+    <div className={ styles.container }>
+      <Head>
+        <title>Expense App</title>
+        <meta name="description"
+          content="A simple expense tracker" />
+      </Head>
 
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Expense Tracker
-          </h1>
-          <NewExpense/>
-          <Expenses items={expenses}/>
-        </main>
+      <main className={ styles.main }>
+        <h1 className={ styles.title }>
+          Expense Tracker
+        </h1>
+        <NewExpense onAddExpense={ addExpenseHandler } />
+        <Expenses items={ expenses } />
+      </main>
 
-        <footer className={styles.footer}>
-          Powered by MJ Linane
-        </footer>
-      </div>
+      <footer className={ styles.footer }>
+        Powered by MJ Linane
+      </footer>
+    </div>
   )
 }
