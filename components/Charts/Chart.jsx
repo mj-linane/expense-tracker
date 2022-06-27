22d94cb45a31d7e1e8c5d38107ae9e2339eb43fe
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-06-27 16:46:39 -0400

    🖥️wip: initial

---
+++ components/Charts/Chart.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './Chart.module.css'
+const Chart = () => {
+  return <div>Charts</div>
+}
+
+export default Chart
---
+++ components/Charts/ChartBar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './ChartBar.module.css'
+
+const ChartBar = () => {
+  return <div>ChartBar</div>
+}
+
+export default ChartBar
