fe0b7eb3a72ed81e7f96b652850bbfb142b8e38c
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-06-27 12:29:36 -0400

    add card styling

--- components/Expenses/ExpenseItem.jsx
+++ components/Expenses/ExpenseItem.jsx
@@ -1,15 +1,28 @@
-import ExpenseDate from './ExpenseDate';
+import { useState } from 'react'
+import Card from '../UI/Card'
+import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title)
+  console.log('ExpenseItem evaluated by React')
+
+  const clickHandler = () => {
+    setTitle('updated')
+    console.log(title)
+  }
+
   return (
-      <div className='expense-item'>
+    <Card>
+      <div className="expense-item">
         <ExpenseDate date={props.date} />
-        <div className='expense-item__description'>
-          <h2>{props.title}</h2>
+        <div className="expense-item__description">
+          <h2>{title}</h2>
         </div>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-  );
+      <button onClick={clickHandler}>Edit Name</button>
+    </Card>
+  )
 }
 
-export default ExpenseItem;
+export default ExpenseItem
