20ff5046db9432f4fe6a190ca14cfcab9fe8dcc7
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-06-27 19:35:40 -0400

    🐞fix: enforce number conversion so multiple values will add as numbers

--- components/NewExpense/ExpenseForm.jsx
+++ components/NewExpense/ExpenseForm.jsx
@@ -27,7 +27,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: Number(enteredAmount),
       date: new Date(enteredDate),
     }
 
