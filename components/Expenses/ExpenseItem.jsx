b16abcc94ff41ac2ca9040bc8e3f36af9249ce0d
MJ Linane <19750620+mj-linane@users.noreply.github.com>
2022-06-27 12:32:41 -0400

    🐞fix: remove example code

--- components/Expenses/ExpenseItem.jsx
+++ components/Expenses/ExpenseItem.jsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
 import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title)
-  console.log('ExpenseItem evaluated by React')
-
-  const clickHandler = () => {
-    setTitle('updated')
-    console.log(title)
-  }
-
   return (
-    <Card>
-      <div className="expense-item">
-        <ExpenseDate date={props.date} />
-        <div className="expense-item__description">
-          <h2>{title}</h2>
-        </div>
-        <div className="expense-item__price">${props.amount}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
       </div>
-      <button onClick={clickHandler}>Edit Name</button>
+      <div className="expense-item__price">${props.amount}</div>
     </Card>
   )
 }
